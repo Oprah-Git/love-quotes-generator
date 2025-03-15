83c6685ee8960bc3defbeeda57b5b0b919bc0308
Oprah-Git <taliweoprah@gmail.com>
2025-03-15 23:59:14 +0200

    Improved the user Experience

--- src/index.js
+++ src/index.js
@@ -10,12 +10,17 @@ function displayQuote(response) {
 
 function generateQuote(event) {
     event.preventDefault();
-
+    
 let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "fbato6d93eb4607e65fcafdf6a781743";
 let context = "You are a fantastic Quote expert and love to write short quotes. Your mission is to generate 4 lines of quotes in basic HTML and separate the lines with <br />. Make sure to follow the user instructions. Do not include a title to the quotes. Sign the quotes with 'Oprah Tee' inside a <strong> element below the quotes and NOT above them";
 let prompt = 'User instructions: Generate a quote about ${instructionsInput.value}'; 
-let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}';
+let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}';
+
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="generating">⏳ Generating quotes about ${instructionsInput.value}</div>`;
+
 
 console.log("Generating quote");
 console.log('Prompt: ${prompt}');
